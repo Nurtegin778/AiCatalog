d02e80ae0ce2c1246555084606e47303e53f5494
Abzal Mametzhan <abamametzan@gmail.com>
2026-03-10 21:01:11 +0500

    f

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import About from "./about";
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
